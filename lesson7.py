e6530d1812ff228cfec2f98c2de74f031d8cb8bb
Sergey Postnikov <ser.post2017@gmail.com>
2021-11-25 16:05:01 +0300

    Красивые аннотации к lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -1,16 +1,43 @@
 # 1) Publisher
+# todo: Разобрать с какими классами работаем, импортировать их
 from rospy import Publisher, init_node, is_shutdown, sleep
 from std_msgs.msg import String
 
-init_node("My_first_node")
+# todo: поднять ноду
+init_node("Имя_ноды")
 
-pub = Publisher("My_first_topic", String, queue_size=20)
+# todo: создать топик (объект издатель)
+pub = Publisher("Имя_топика", Класс/Тип_сообщения, размер очереди)
 
-s = String()
-s.data = "Hello, truecoders!"
+# todo: создать объект сообщения и закинуть сообщение в него
+msg = String()
+msg.data = "Сообщение в топик"
 
-# print(dir(Publisher))
-
+# todo: Рабочий цикл
 while not is_shutdown():
-    pub.publish(s)
-    # sleep(1)
+	объект_издатель.publish(объект_строки)
+	sleep(1) 
+
+
+
+
+# 2)Suscriber
+# todo: Разобрать с какими классами/функциями работаем, импортировать их
+
+from rospy import init_node, spin
+from rospy.topics import Subscriber
+
+from std_msgs.msg import String
+
+# todo: поднять ноду
+init_node("Имя_ноды")
+
+# todo: функция обработки сообщения
+def f(msg):
+	print(msg.data)
+
+# todo: создать объект Подписчик
+sub = Subscriber("Имя_топика_Издателя", Класс/Тип_сообщения, функция_обработки_сообщения)
+
+# todo: крутимся
+spin()
